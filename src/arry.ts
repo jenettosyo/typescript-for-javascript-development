cc686f17aed89b142eec06f549845d9bfc9f905c
syoyokoi0328 <s.yokoi@gemcook.com>
2020-05-25 13:55:39 +0900

    study array,tuple-type

---
+++ src/arry.ts
@@ -0,0 +1,18 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+console.log(numbers);
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings2: Array<string> = ['tokyo', 'kyoto', 'osaka'];
+
+let strings: string[] = ['Typescript', 'Javascript', 'React'];
+
+let nijigenHairetsu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'japan'];
+
+console.log(hairetsu);
---
+++ src/tuple.ts
@@ -0,0 +1,5 @@
+export {};
+
+let profile: [string, number] = ['syo', 19];
+// profile = [19, 'syo'];
+console.log(profile);
