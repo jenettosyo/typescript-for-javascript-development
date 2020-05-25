fd3f33202204b3d24d4cd95821510452a1a3f96e
syoyokoi0328 <s.yokoi@gemcook.com>
2020-05-25 16:32:24 +0900

    study interface

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export {};
+
+type ObjectType = {
+  name: string;
+  age: number;
+};
+
+interface Objectinterface {
+  name: string;
+  age: number;
+}
+
+let object: Objectinterface = {
+  name: 'syo',
+  age: 19,
+};
