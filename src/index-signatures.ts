68d4b8ec20d16df5f84d4f238aa09aec2e20bdfd
syoyokoi0328 <s.yokoi@gemcook.com>
2020-05-26 16:58:34 +0900

    study indexsignatures

---
+++ src/index-signatures.ts
@@ -0,0 +1,13 @@
+export {};
+
+interface Profile {
+  name: string;
+  underTwenty: boolean;
+  [index: string]: string | number | boolean;
+}
+
+let profile: Profile = { name: 'sho', underTwenty: false };
+
+profile.name = 'sho';
+profile.age = 43;
+profile.nationality = 'Japan';
